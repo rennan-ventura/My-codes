f585beee1d2ad3f5cf343f10a46379c7be33bbcf
Rennan <rennan.ventura.alves@gmail.com>
2022-04-20 20:36:16 -0300

    Transformando Books em pageable e melhorando a segurança do codigo

--- mercado-livro/mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -2,8 +2,9 @@ package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
+import com.mercadolivro.controller.response.CustomerResponse
 import com.mercadolivro.extension.toCustomerModel
-import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.extension.toResponse
 import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -14,8 +15,8 @@ class CustomerController(
     val customerService : CustomerService
 ) {
     @GetMapping
-    fun getAll(@RequestParam name: String?): List<CustomerModel> {
-        return customerService.getAll(name)
+    fun getAll(@RequestParam name: String?): List<CustomerResponse> {
+        return customerService.getAll(name).map { it.toResponse() }
     }
 
     @PostMapping
@@ -25,14 +26,15 @@ class CustomerController(
     }
 
     @GetMapping("/{id}")
-    fun getCustomer(@PathVariable id: Int): CustomerModel{
-        return customerService.findById(id)
+    fun getCustomer(@PathVariable id: Int): CustomerResponse{
+        return customerService.findById(id).toResponse()
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun updateCustomer(@PathVariable id: Int, @RequestBody customer: PutCustomerRequest){
-        customerService.updateCustomer(customer.toCustomerModel(id))
+        val customerSaved = customerService.findById(id)
+        customerService.updateCustomer(customer.toCustomerModel(customerSaved))
     }
 
     @DeleteMapping("/{id}")
--- mercado-livro/mercado-livro/src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
+++ mercado-livro/mercado-livro/src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.model
 
+import com.mercadolivro.enums.CustomerStatus
 import javax.persistence.*
 
 @Entity(name = "customer")
@@ -12,5 +13,9 @@ class CustomerModel(
     var name: String,
 
     @Column
-    var email: String
+    var email: String,
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: CustomerStatus
 )
