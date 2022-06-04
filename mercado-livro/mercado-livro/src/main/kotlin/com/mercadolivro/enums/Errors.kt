363650d42e9ec89c7dea0e6f5113b72de2585019
Rennan <rennan.ventura.alves@gmail.com>
2022-06-03 23:09:02 -0300

    terminando as tratativas de erro

---
+++ mercado-livro/mercado-livro/src/main/kotlin/com/mercadolivro/enums/Errors.kt
@@ -0,0 +1,9 @@
+package com.mercadolivro.enums
+
+enum class Errors(val code: String, val message: String) {
+
+    ML101("ML-101", "Book [%s] not exists"),
+    ML102("ML-102", "Cannot upate book with status [%s]"),
+    ML201("ML-201", "Customer [%s] not exists"),
+
+}
