ee5b1c3b2e2a6345c4978b2cf466a6b533ec746a
Rennan <rennan.ventura.alves@gmail.com>
2022-07-20 22:53:47 -0300

    Adicionando a camada secyrity

--- mercado-livro/mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -13,7 +13,7 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("customers")
 class CustomerController(
-    val customerService : CustomerService
+    private val customerService : CustomerService
 ) {
     @GetMapping
     fun getAll(@RequestParam name: String?): List<CustomerResponse> {
